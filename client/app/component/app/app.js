8f780c28d7028259de099bec558f680a7fe04700
mstaniuk <mateusz.staniuk@gmail.com>
2018-08-03 23:23:08 +0200

    Changes in layout

--- client/app/component/app/app.js
+++ client/app/component/app/app.js
@@ -7,17 +7,26 @@ import Connection from "../connection/connection.js";
 import Container from "../container/container.js";
 import Grid from "../grid/grid.js";
 import Section from "../section/section.js";
+import Header from "../header/header.js";
 
 const getRooms = rooms => rooms.map(room => <Room {...room} key={room.id} />);
 
 export default (state, actions) => (
-  <Container>
-    <h1>Play Room</h1>
-    <Section>
-      <Connection />
-    </Section>
-    <Section>
-      <Grid>{getRooms(state.rooms)}</Grid>
-    </Section>
-  </Container>
+  <div>
+    <Header>
+      <Container>
+        <h1>Cool app name</h1>
+      </Container>
+    </Header>
+    <main>
+      <Container>
+        <Section>
+          <Connection />
+        </Section>
+        <Section>
+          <Grid>{getRooms(state.rooms)}</Grid>
+        </Section>
+      </Container>
+    </main>
+  </div>
 );
---
+++ client/app/component/header/header.js
@@ -0,0 +1,5 @@
+import { h } from "hyperapp";
+
+import "./header.scss";
+
+export default (props, children) => <header class="header">{children}</header>;
